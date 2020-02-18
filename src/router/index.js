003c0ade612997236d5b41d8277a2c029b01a29d
Riddle-Ghost <test@test.com>
2020-02-18 10:23:34 +0300

    Add routes and delete nav links

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    meta: {layout: 'main'},
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
   },
   {
     path: '/login',
@@ -18,11 +18,47 @@ const routes = [
     component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   },
   {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import(/* webpackChunkName: "register" */ '../views/Register.vue')
+  },
+  {
     path: '/categories',
     name: 'categories',
     meta: {layout: 'main'},
     component: () => import(/* webpackChunkName: "categories" */ '../views/Categories.vue')
-  }
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import(/* webpackChunkName: "history" */ '../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import(/* webpackChunkName: "planning" */ '../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import(/* webpackChunkName: "profile" */ '../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import(/* webpackChunkName: "record" */ '../views/Record.vue')
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: {layout: 'main'},
+    component: () => import(/* webpackChunkName: "detail" */ '../views/DetailRecord.vue')
+  },
 
 ]
 
